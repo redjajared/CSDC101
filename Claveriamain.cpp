3e0b9a8710d2a4dbb015962e82d6a742d228016f
redjajared <jclaveria@gbox.adnu.edu.ph>
2025-08-08 14:11:28 +0800

    Create Claveriamain.cpp
    Added main code about simple calculator.

---
+++ Claveriamain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int num1, num2, product;// variables 
+
+cout << "Enter number: ";//enter input num1
+cin >> num1;
+cout << "Enter another number: ";//enter input num2
+cin >> num2;
+product = num1 * num2;//set the value of sum as num1 and num2
+cout << product;
+cout << " The product of the two numbers is: "<<product;//output of the two numbers
+
+	return 0;
+}
